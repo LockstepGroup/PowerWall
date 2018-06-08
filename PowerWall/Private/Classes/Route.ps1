873550f05cde041d77e49c811358b949e2fd1138
Brian Addicks <brian@addicks.us>
2018-06-08 14:55:44 -0400

    started work on Route.GetPaloAltoCommand

--- PowerWall/Private/Classes/Route.ps1
+++ PowerWall/Private/Classes/Route.ps1
@@ -3,4 +3,21 @@ Class Route {
     [string]$Interface
     [string]$NextHop
     [string]$Metric
+    [string]$VirtualRouter
+
+    [string] GetPaloAltoCommand([String]$VirtualRouter) {
+        #set network virtual-router nitel routing-table ip static-route ROUTENAME interface INTERFACE destination 1.1.1.1/1 nexthop ip-address 2.2.2.2
+        $Command = "set network virtual-router "
+        $Command += $VirtualRouter
+        $Command += " routing-table ip static-route "
+        $Command += $RouteName
+        $Command += " interface "
+        $Command += $this.Interface
+        $Command += " destination "
+        $Command += $this.destination
+        $Command += " nexthop ip-address "
+        $Command += $this.NextHop
+
+        return $Command
+    }
 }
