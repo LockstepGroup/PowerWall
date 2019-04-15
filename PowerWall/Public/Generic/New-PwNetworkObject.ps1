5c2f22af0bf918955ab2f5a0a209bb332c6c92d2
Brian Addicks <brian@addicks.us>
2019-04-15 12:36:47 -0400

    changed names

---
+++ PowerWall/Public/Generic/New-PwNetworkObject.ps1
@@ -0,0 +1,14 @@
+function New-PwNetworkObject {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $True, Position = 0)]
+        [string]$Name
+    )
+
+    # It's nice to be able to see what cmdlet is throwing output isn't it?
+    $VerbosePrefix = "New-NetworkObject:"
+
+    $NewObject = [NetworkObject]::new()
+    $NewObject.Name = $Name
+    $NewObject
+}
---
+++ PowerWall/Public/Generic/New-PwServiceObject.ps1
@@ -0,0 +1,14 @@
+function New-PwServiceObject {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $True, Position = 0)]
+        [string]$Name
+    )
+
+    # It's nice to be able to see what cmdlet is throwing output isn't it?
+    $VerbosePrefix = "New-ServiceObject:"
+
+    $NewObject = [ServiceObject]::new()
+    $NewObject.Name = $Name
+    $NewObject
+}
